4c6298823bab306bfb527b46123004e2cca49037
Atsushi Watanabe <atsushi.w@ieee.org>
2016-01-26 11:32:23 +0900

    Added joint id error check in joint control commands

--- src/command_joint.c
+++ src/command_joint.c
@@ -21,35 +21,61 @@
 #include <param.h>
 #include <control.h>
 #include <command.h>
+#include <yprintf.h>
 
 /* ライブラリ用 */
 #include <ypspur.h>
 
 void joint_torque_com( int id, double *data, SpurUserParamsPtr spur )
 {
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	spur->torque[id] = data[0];
 	spur->wheel_mode[id] = MOTOR_CONTROL_FREE;
 }
 
 void joint_vel_com( int id, double *data, SpurUserParamsPtr spur )
 {
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	spur->wvelref[id] = data[0];
 	spur->wheel_mode[id] = MOTOR_CONTROL_VEL;
 }
 
 void joint_ang_com( int id, double *data, SpurUserParamsPtr spur )
 {
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	spur->wheel_angle[id] = data[0];
 	spur->wheel_mode[id] = MOTOR_CONTROL_ANGLE;
 }
 
 void set_joint_accel_com( int id, double *data, SpurUserParamsPtr spur )
 {
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	spur->wheel_accel[id] = data[0];
 }
 
 void set_joint_vel_com( int id, double *data, SpurUserParamsPtr spur )
 {
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	spur->wheel_vel[id] = data[0];
 }
 
@@ -57,6 +83,11 @@ void get_joint_vel_com( int id, double *data, SpurUserParamsPtr spur )
 {
 	OdometryPtr odometry;
 	
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	odometry = get_odometry_ptr(  );
 
 	data[0] = odometry->wvel[id];
@@ -67,6 +98,11 @@ void get_joint_vref_com( int id, double *data, SpurUserParamsPtr spur )
 {
 	OdometryPtr odometry;
 	
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	odometry = get_odometry_ptr(  );
 
 	data[0] = spur->wvelref[id];
@@ -77,6 +113,11 @@ void get_joint_ang_com( int id, double *data, SpurUserParamsPtr spur )
 {
 	OdometryPtr odometry;
 	
+	if( id > YP_PARAM_MAX_MOTOR_NUM )
+	{
+		yprintf(OUTPUT_LV_ERROR, "Motor id out of range (%d)\n", id);
+		return;
+	}
 	odometry = get_odometry_ptr(  );
 
 	data[0] = odometry->wang[id];

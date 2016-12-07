45ab93e3f2f42594b9fd586c4a67bc9859e4867a
Atsushi Watanabe <atsushi.w@ieee.org>
2016-12-07 17:29:41 +0900

    adds fixed point value underflow for old device

--- src/param.c
+++ src/param.c
@@ -1244,7 +1244,16 @@ void set_param_velocity( void )
 		}
 		ffr = 256.0 * 2.0 * M_PI * g_P[YP_PARAM_TORQUE_UNIT][0] / ( g_P[YP_PARAM_COUNT_REV][0] * fabs(g_P[YP_PARAM_GEAR][0]) );
 		if( ischanged_p(YP_PARAM_GAIN_A,0) || ffr_changed )
+		{
+			yprintf( OUTPUT_LV_PARAM, "Info: GAIN_A: %d\n", 
+					(int)(g_P[YP_PARAM_GAIN_A][0] * ffr) );
+			if( abs(g_P[YP_PARAM_GAIN_A][0] * ffr) < 2 )
+			{
+				yprintf( OUTPUT_LV_ERROR, "ERROR: GAIN_A fixed point value underflow\n" );
+				yprintf( OUTPUT_LV_ERROR, "ERROR: Decrease TORQUE_FINENESS[%d]\n", 0 );
+			}
 			parameter_set( PARAM_p_A, 0, g_P[YP_PARAM_GAIN_A][0] * ffr );
+		}
 		if( ischanged_p(YP_PARAM_GAIN_C,0) || ffr_changed )
 			parameter_set( PARAM_p_C, 0, g_P[YP_PARAM_GAIN_C][0] * ffr );
 		if( ischanged_p(YP_PARAM_GAIN_E,0) || ffr_changed )
@@ -1258,7 +1267,16 @@ void set_param_velocity( void )
 		}
 		ffl = 256.0 * 2.0 * M_PI * g_P[YP_PARAM_TORQUE_UNIT][1] / ( g_P[YP_PARAM_COUNT_REV][1] * fabs(g_P[YP_PARAM_GEAR][1]) );
 		if( ischanged_p(YP_PARAM_GAIN_B,0) || ffl_changed )
+		{
+			yprintf( OUTPUT_LV_PARAM, "Info: GAIN_B: %d\n", 
+					(int)(g_P[YP_PARAM_GAIN_A][0] * ffl) );
+			if( abs(g_P[YP_PARAM_GAIN_B][0] * ffl) < 2 )
+			{
+				yprintf( OUTPUT_LV_ERROR, "ERROR: GAIN_B fixed point value underflow\n" );
+				yprintf( OUTPUT_LV_ERROR, "ERROR: Decrease TORQUE_FINENESS[%d]\n", 1 );
+			}
 			parameter_set( PARAM_p_B, 0, g_P[YP_PARAM_GAIN_B][0] * ffl );
+		}
 		if( ischanged_p(YP_PARAM_GAIN_D,0) || ffl_changed )
 			parameter_set( PARAM_p_D, 0, g_P[YP_PARAM_GAIN_D][0] * ffl );
 		if( ischanged_p(YP_PARAM_GAIN_F,0) || ffl_changed )

c982161342947232e9a4651f0c3fe77f6c2669fd
Atsushi Watanabe <atsushi.w@ieee.org>
2017-07-29 23:17:46 +0900

    Fix include.

--- cmake/modules/FindSSM.cmake
+++ cmake/modules/FindSSM.cmake
@@ -3,17 +3,13 @@
 find_path(SSM_INCLUDE_DIRS ssm.h
 	HINTS ENV SSM_DIR
 	PATH_SUFFIXES include
-	PATHS /usr /usr/local
 )
 find_library(SSM_LIBRARIES
 	NAMES ssm
 	HINTS ENV SSM_DIR
-	PATH_SUFFIXES lib lib64
-	PATHS /usr /usr/local
 )
 find_program(SSM_EXECUTABLE
 	NAMES ssm-coordinator
-	PATHS /usr/bin /usr/local/bin
 )
 
 include(FindPackageHandleStandardArgs)

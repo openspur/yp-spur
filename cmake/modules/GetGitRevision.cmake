b8d6e4c4206ef41c5e3f84990c1424ba4b50312f
Shane Loretz <sloretz@openrobotics.org>
2020-07-30 09:41:26 -0700

    Fallback message if unable to get git revision
    Proposal to fix http://build.ros.org/job/Nbin_uF64__ypspur__ubuntu_focal_amd64__binary/5/
    
    Uses `-C <path>` when calling `git rev-parse`
    Set failure message when unable to get a git revision

--- cmake/modules/GetGitRevision.cmake
+++ cmake/modules/GetGitRevision.cmake
@@ -11,14 +11,22 @@ function(get_git_revision)
 		set(GIT_REVISION "Failed to get git revision." PARENT_SCOPE)
 	endif(NOT GIT)
 
-	exec_program(${GIT} ${CMAKE_CURRENT_SOURCE_DIR}
+	exec_program(${GIT} -C "${CMAKE_CURRENT_SOURCE_DIR}"
 		ARGS "rev-parse HEAD"
 		OUTPUT_VARIABLE REVISION
+		RETURN_VALUE rev_parse_ret
 	)
-	exec_program(${GIT} ${CMAKE_CURRENT_SOURCE_DIR}
+	exec_program(${GIT} -C "${CMAKE_CURRENT_SOURCE_DIR}"
 		ARGS "rev-parse --short HEAD"
 		OUTPUT_VARIABLE REVISION_SHORT
+		RETURN_VALUE rev_parse_short_ret
 	)
+
+	# Might not be building from a git repo
+	if((NOT ${rev_parse_ret} EQUAL "0") OR (NOT ${rev_parse_short_ret} EQUAL "0"))
+		set(GIT_REVISION_SHORT "Snapshot")
+		set(GIT_REVISION "Failed to get git revision.")
+	endif()
 
 	set(GIT_REVISION_SHORT ${REVISION_SHORT} PARENT_SCOPE)
 	set(GIT_REVISION ${REVISION} PARENT_SCOPE)

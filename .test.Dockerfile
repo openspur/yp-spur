0ac580cf0f61f3264d641b48736c7672b2751e2f
Atsushi Watanabe <atsushi.w@ieee.org>
2020-07-31 11:50:21 +0900

    Fallback message if unable to get git revision (#142)
    Fix http://build.ros.org/job/Nbin_uF64__ypspur__ubuntu_focal_amd64__binary/5/
    
    Uses `-C <path>` when calling `git rev-parse`
    Set failure message when unable to get a git revision
    
    Co-authored-by: Shane Loretz <sloretz@openrobotics.org>

--- .test.Dockerfile
+++ .test.Dockerfile
@@ -4,7 +4,11 @@ FROM ubuntu:${UBUNTU_VERSION}
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get -qq update \
-  && apt-get install -y --no-install-recommends build-essential cmake libgtest-dev
+  && apt-get install -y --no-install-recommends \
+    build-essential \
+    cmake \
+    git \
+    libgtest-dev
 # don't clean apt-cache to reuse later
 
 ENV CTEST_OUTPUT_ON_FAILURE=1
@@ -29,6 +33,7 @@ RUN apt-get install -y --no-install-recommends libreadline-dev
 
 RUN mkdir -p /yp-spur/build-readline
 WORKDIR /yp-spur/build-readline
-RUN cmake .. && make -j && make test && make install
+RUN cmake .. && make -j && make test && make install && ldconfig
 # check that ypspur-interpreter is built with readline
 RUN ldd ypspur-interpreter | grep libreadline.so
+RUN ypspur-coordinator --version
--- cmake/modules/GetGitRevision.cmake
+++ cmake/modules/GetGitRevision.cmake
@@ -2,25 +2,33 @@
 
 function(get_git_revision)
 
-	find_program(GIT
-		NAMES git
-	)
+  find_program(GIT
+    NAMES git
+  )
 
-	if(NOT GIT)
-		set(GIT_REVISION_SHORT "Snapshot" PARENT_SCOPE)
-		set(GIT_REVISION "Failed to get git revision." PARENT_SCOPE)
-	endif(NOT GIT)
+  if(NOT GIT)
+    set(GIT_REVISION_SHORT "Snapshot" PARENT_SCOPE)
+    set(GIT_REVISION "Failed to get git revision." PARENT_SCOPE)
+  else()
+    exec_program(${GIT} -C "${CMAKE_CURRENT_SOURCE_DIR}"
+      ARGS "rev-parse HEAD"
+      OUTPUT_VARIABLE REVISION
+      RETURN_VALUE rev_parse_ret
+    )
+    exec_program(${GIT} -C "${CMAKE_CURRENT_SOURCE_DIR}"
+      ARGS "rev-parse --short HEAD"
+      OUTPUT_VARIABLE REVISION_SHORT
+      RETURN_VALUE rev_parse_short_ret
+    )
 
-	exec_program(${GIT} ${CMAKE_CURRENT_SOURCE_DIR}
-		ARGS "rev-parse HEAD"
-		OUTPUT_VARIABLE REVISION
-	)
-	exec_program(${GIT} ${CMAKE_CURRENT_SOURCE_DIR}
-		ARGS "rev-parse --short HEAD"
-		OUTPUT_VARIABLE REVISION_SHORT
-	)
-
-	set(GIT_REVISION_SHORT ${REVISION_SHORT} PARENT_SCOPE)
-	set(GIT_REVISION ${REVISION} PARENT_SCOPE)
+    message(WARNING "${rev_parse_ret} ${rev_parse_short_ret}")
+    if((NOT ${rev_parse_ret} EQUAL "0") OR (NOT ${rev_parse_short_ret} EQUAL "0"))
+      set(GIT_REVISION_SHORT "Snapshot" PARENT_SCOPE)
+      set(GIT_REVISION "Failed to get git revision." PARENT_SCOPE)
+    else()
+      set(GIT_REVISION_SHORT ${REVISION_SHORT} PARENT_SCOPE)
+      set(GIT_REVISION ${REVISION} PARENT_SCOPE)
+    endif()
+  endif()
 
 endfunction(get_git_revision)
